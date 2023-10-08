d4558685a2f17f6d1d516a9ce13ebd360b2e07f3
Ivan Lintsov <ivan.linzov@gmail.com>
2023-10-08 14:28:48 +0300

    Корректировка форматирования файлов

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -30,7 +30,8 @@ class TestCalcRating:
         }
         return data, rating_scores
 
-    def test_init_calc_rating(self, input_data: tuple[DataType, RatingsType]) -> None:
+    def test_init_calc_rating(self, input_data: tuple[DataType,
+                                                      RatingsType]) -> None:
         calc_rating = CalcRating(input_data[0])
         assert input_data[0] == calc_rating.data
 
@@ -38,4 +39,5 @@ class TestCalcRating:
         rating = CalcRating(input_data[0]).calc()
         for student in rating.keys():
             rating_score = rating[student]
-        assert pytest.approx(rating_score, abs=0.001) == input_data[1][student]
+        assert pytest.approx(rating_score,
+                             abs=0.001) == input_data[1][student]

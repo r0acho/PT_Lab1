37a973a4bf9a279e17a11a962b7a50511c8f388f
test <iv>
2023-10-13 16:28:16 +0300

    Реализовано получение ридера в зависимости от расширения

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import argparse
 import sys
+from DataReader import DataReader
+import os
 
 from StudentAnalyzer import StudentAnalyzer
+from TextDataReader import TextDataReader
 from XmlDataReader import XmlDataReader
 
 
@@ -14,9 +17,21 @@ def get_path_from_arguments(args) -> str:
     return args.path
 
 
+def get_current_reader(path: str) -> DataReader:
+    _, file_extension = os.path.\
+        splitext(path)
+    match file_extension:
+        case ".txt":
+            return TextDataReader()
+        case ".xml":
+            return XmlDataReader()
+        case _:
+            raise ValueError("Неподдерживаемый формат")
+
+
 def main():
     path = get_path_from_arguments(sys.argv[1:])
-    reader = XmlDataReader()
+    reader = get_current_reader(path)
     students = reader.read(path)
     print("Students: ", students)
     first_quartile_students = StudentAnalyzer().\
--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-from src.main import get_path_from_arguments
+from TextDataReader import TextDataReader
+from XmlDataReader import XmlDataReader
+from src.main import get_path_from_arguments, get_current_reader
 import pytest
 
 
 @pytest.fixture()
-def correct_arguments_string() -> tuple[list[str], str]:
+def correct_arguments_string_txt() -> tuple[list[str], str]:
     return ["-p", "/home/user/file.txt"], "/home/user/file.txt"
 
+@pytest.fixture()
+def correct_arguments_string_xml() -> tuple[list[str], str]:
+    return ["-p", "/home/user/file.txt"], "/home/user/file.xml"
 
 @pytest.fixture()
 def noncorrect_arguments_string() -> list[str]:
@@ -14,9 +19,9 @@ def noncorrect_arguments_string() -> list[str]:
 
 
 def test_get_path_from_correct_arguments(
-        correct_arguments_string: tuple[list[str], str]) -> None:
-    path = get_path_from_arguments(correct_arguments_string[0])
-    assert path == correct_arguments_string[1]
+        correct_arguments_string_txt: tuple[list[str], str]) -> None:
+    path = get_path_from_arguments(correct_arguments_string_txt[0])
+    assert path == correct_arguments_string_txt[1]
 
 
 def test_get_path_from_noncorrect_arguments(
@@ -25,3 +30,13 @@ def test_get_path_from_noncorrect_arguments(
         get_path_from_arguments(noncorrect_arguments_string[0])
 
     assert e.type == SystemExit
+
+def get_correct_reader_txt(
+        correct_arguments_string_txt: tuple[list[str], str]) -> None:
+    path = get_path_from_arguments(correct_arguments_string_txt[0])
+    assert TextDataReader == get_current_reader(path)
+
+def get_correct_reader_xml(
+        correct_arguments_string_xml: tuple[list[str], str]) -> None:
+    path = get_path_from_arguments(correct_arguments_string_xml[0])
+    assert XmlDataReader == get_current_reader(path)
